6c048dac2e9536a79104e8976b541397b0c73ef6
Alex <ovcinikov@gmail.com>
2021-02-26 21:25:36 +0000

    Added state switching for scene objects
    Added scripts that allows to turn on/off objects or change their shader switch (property name specific switch)

---
+++ GJLGameParadeSpring2021/Assets/Scripts/Player/PlayerSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerSwitch : MonoBehaviour
+    {
+        [SerializeField] private KeyCode switchStateButton;
+        [SerializeField] private float keyPressTimer;
+        private StateSwitch[] objectsWithSwitch;
+        private float timeSincePressed;
+        void Awake()
+        {
+            objectsWithSwitch = FindObjectsOfType<StateSwitch>();
+        }
+    
+        void Update()
+        {
+            timeSincePressed += Time.deltaTime;
+            if (timeSincePressed < keyPressTimer) return;
+            if (!Input.GetKeyDown(switchStateButton)) return;
+            ChangeColors();
+            timeSincePressed = 0;
+        }
+
+        private void ChangeColors()
+        {
+            foreach (var gameObj in objectsWithSwitch)
+            {
+                gameObj.Switch();
+            }
+        }
+    }
+}
---
+++ GJLGameParadeSpring2021/Assets/Scripts/Player/StateSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class StateSwitch : MonoBehaviour
+{
+    [SerializeField] private StateType stateType;
+    [Tooltip("Variable used for VisibilitySwitch to know if objects is active on start")]
+    [SerializeField] private bool isActive;
+    private Material material;
+    private readonly int Switch1 = Shader.PropertyToID("_switch");
+
+    private enum StateType
+    {
+        MaterialSwitch,
+        VisibilitySwitch
+    }
+
+    void Start()
+    {
+        material = stateType == StateType.MaterialSwitch ? GetComponent<MeshRenderer>().material : null;
+        if (stateType != StateType.VisibilitySwitch) return;
+        if (!isActive) gameObject.SetActive(isActive);
+        }
+
+    public void Switch()
+    {
+        switch (stateType)
+        {
+            case StateType.MaterialSwitch:
+                var value = material.GetFloat(Switch1);
+                value = value == 0 ? 1 : 0;
+                material.SetFloat(Switch1,value);
+                break;
+            case StateType.VisibilitySwitch:
+                isActive = !isActive;
+                gameObject.SetActive(isActive);
+                break;
+        }
+    }
+}
