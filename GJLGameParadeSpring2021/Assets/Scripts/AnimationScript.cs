d506f2acf769b3cd7a5f1e4250f7c5f70188ef64
dave <dnewman.1912@gmail.com>
2021-02-27 22:23:10 +0000

    Added 3d model and animations for Baabra

---
+++ GJLGameParadeSpring2021/Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    private Animator sheepAnim1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sheepAnim1 = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+
+        //walking\\
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            sheepAnim1.CrossFade("walking", 0.1f);
+            sheepAnim1.SetBool("isWalking", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            sheepAnim1.SetBool("isIdle", true);
+            sheepAnim1.SetBool("isWalking", false);
+            sheepAnim1.CrossFade("Idle", 0.1f);
+        }
+
+
+        //placeholder for testing the character selection animations\\
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            sheepAnim1.CrossFade("CS no", 0.1f);
+            sheepAnim1.SetBool("isPicked", true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            sheepAnim1.SetBool("isIdle", true);
+            sheepAnim1.SetBool("isPicked", false);
+            sheepAnim1.CrossFade("Idle", 0.1f);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            sheepAnim1.CrossFade("CS yes", 0.1f);
+            sheepAnim1.SetBool("isNotPicked", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            sheepAnim1.SetBool("isIdle", true);
+            sheepAnim1.SetBool("isNotPicked", false);
+            sheepAnim1.CrossFade("Idle", 0.1f);
+        }
+
+
+
+    }
+    // needs to merge this into lizzies character controller as input will affect the triggers\\
+}
