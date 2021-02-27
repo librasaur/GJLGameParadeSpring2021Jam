9f485b124f3398ea15e5f9dfa412372a0322f3d4
dave <dnewman.1912@gmail.com>
2021-02-27 15:49:57 +0000

    added new UI elements
    added title screen scene and pause menu base functionality

---
+++ GJLGameParadeSpring2021/Assets/Scripts/UI scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Canvas can1;
+
+    private void Start()
+    {
+        can1.gameObject.SetActive(false);
+
+
+    }
+    
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+            can1.gameObject.SetActive(true); 
+
+        }
+    }
+
+
+  
+
+
+
+    public void sceneSwitch(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
