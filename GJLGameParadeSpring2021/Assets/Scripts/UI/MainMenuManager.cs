04886ff4a5cd6067b9ad322870841cdf82d52ae5
Lizzie <lizzie.briggs10@gmail.com>
2021-02-28 13:02:55 +0000

    Implemented main menu and level selection

---
+++ GJLGameParadeSpring2021/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        public void LoadScene(string levelName)
+        {
+            SceneManager.LoadScene(levelName);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        public void OpenSidePanel(GameObject panel)
+        {
+            panel.SetActive(true);
+        }
+
+        public void CloseSidePanel(GameObject panel)
+        {
+            
+        }
+    }
+}
